aa5943b388db3af851f59a92510917d879b0e29c
Renaud Lepage <root@cybikbase.com>
2019-09-08 12:40:33 -0700

    Trying to compartmentalize and pipeline even further

---
+++ confstrap.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+TARGET="${1}"
+IS_DEBUG="false"
+[ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
+
+cd angle
+gn gen out/${TARGET}
+cat >> out/${TARGET}/args.gn << EOF
+target_cpu = "x64"
+is_debug = $IS_DEBUG
+angle_enable_gl = false
+angle_enable_null = false
+EOF
+gn gen out/${TARGET}
