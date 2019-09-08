700b8d943c26f08c7e422c1f1b08e8eed091c354
Renaud Lepage <root@cybikbase.com>
2019-09-08 09:06:37 -0700

    Understood what I was doing woefully wrong. Also, abstractions :D

--- bootstrap-debug.sh
+++ bootstrap-debug.sh
@@ -1,14 +1,3 @@
 #! /bin/bash
 
-echo TARGET="Debug"
-export PATH=`pwd`/depot_tools:$PATH
-./bootstrap.sh
-cd angle
-gn gen out/$TARGET
-cat >> out/$TARGET/args.gn << EOF
-target_cpu = "x64"
-is_debug = true
-angle_enable_gl = false
-angle_enable_null = false
-EOF
-gn gen out/$TARGET
+./bootstrap-moded.sh Debug
