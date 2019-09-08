4ca0e6a4420f58cdb49768f22b69a733523ce3c5
Renaud Lepage <root@cybikbase.com>
2019-09-08 08:51:19 -0700

    recorrect

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
+
+# tzdata override
+sudo bash -c 'ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime'
+sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt -y install tzdata'
+
+# Cloning
 git clone https://chromium.googlesource.com/angle/angle
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 export DEPOT_TOOLS=`pwd`/depot_tools
-
 export PATH=$DEPOT_TOOLS:$PATH
 cd angle
 scripts/bootstrap.py
 gclient sync
-
-# tzdata override
-ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt -y install tzdata
-
 sudo ./build/install-build-deps.sh
