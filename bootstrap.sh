5d79ccd01c1435d75d75a4a39a3c36daee477c40
Renaud Lepage <root@cybikbase.com>
2019-09-08 12:16:21 -0700

    Rework gitlab again, less copypasta problems

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# tzdata override
-sudo bash -c 'ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime'
-sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt -y install tzdata'
-
 # Cloning
 git clone https://chromium.googlesource.com/angle/angle
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
