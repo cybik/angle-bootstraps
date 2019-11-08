dfdad882ea4c0edcb17c88824e7a11c8d2080aeb
Renaud Lepage <cybik@users.noreply.github.com>
2019-11-08 08:46:57 -0800

    ggp enablement (2)

--- confstrap.sh
+++ confstrap.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
 TARGET="${1}"
+IS_GGP="false"
+IS_LINUX="true"
 IS_DEBUG="false"
+
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
+[ ! -z "${2}" ] && IS_GGP="true"
+[ ! -z "${2}" ] && IS_LINUX="false"
 
 cd angle
 gn gen out/${TARGET}
@@ -11,5 +16,6 @@ target_cpu = "x64"
 is_debug = $IS_DEBUG
 angle_enable_gl = false
 angle_enable_null = false
+is_ggp = $IS_GGP
 EOF
 gn gen out/${TARGET}
