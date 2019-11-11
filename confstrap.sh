a7aa5370737e683692b33cddaf54062fc14fe8b8
Ethan Lee <flibitijibibo@gmail.com>
2019-11-11 14:20:50 -0500

    Force Clang for GGP, I think this will do it?

--- confstrap.sh
+++ confstrap.sh
@@ -4,11 +4,15 @@ TARGET="${1}"
 IS_GGP="false"
 IS_LINUX="true"
 IS_DEBUG="false"
+OUR_CC="gcc"
+OUR_CXX="g++"
 
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
     IS_GGP="true"
     IS_LINUX="false"
+    OUR_CC="clang"
+    OUR_CXX="clang++ -stdlib=libc++"
 fi
 
 cd angle
@@ -19,5 +23,7 @@ is_debug = $IS_DEBUG
 angle_enable_gl = false
 angle_enable_null = false
 is_ggp = $IS_GGP
+cc = $OUR_CC
+cxx = $OUR_CXX
 EOF
 gn gen out/${TARGET}
