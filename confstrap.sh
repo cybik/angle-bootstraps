01c8679c393065f286ec399da0d9e229482492b8
Ethan Lee <flibitijibibo@gmail.com>
2019-11-11 17:40:08 -0500

    Missing "

--- confstrap.sh
+++ confstrap.sh
@@ -14,4 +14,4 @@ fi
 cd angle
 gn gen out/${TARGET}
 # TODO: -stdlib=libc++
-gn args out/${TARGET} --args="target_cpu=\"x64\" is_debug=$IS_DEBUG angle_enable_gl=false angle_enable_null=false angle_enable_vulkan=true is_ggp=$IS_GGP is_linux=$IS_LINUX
+gn args out/${TARGET} --args="target_cpu=\"x64\" is_debug=$IS_DEBUG angle_enable_gl=false angle_enable_null=false angle_enable_vulkan=true is_ggp=$IS_GGP is_linux=$IS_LINUX"
