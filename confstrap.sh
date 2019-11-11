af91e91f7fbe87f9c850f27ca928c623894a427c
Ethan Lee <flibitijibibo@gmail.com>
2019-11-11 17:38:16 -0500

    Use gn args to set parameters

--- confstrap.sh
+++ confstrap.sh
@@ -13,23 +13,5 @@ fi
 
 cd angle
 gn gen out/${TARGET}
-gn args out/${TARGET} --list
-cat >> out/${TARGET}/args.gn << EOF
-declare_args() {
-    target_cpu = "x64"
-    is_debug = $IS_DEBUG
-    angle_enable_gl = false
-    angle_enable_null = false
-    angle_enable_vulkan = true
-    is_ggp = $IS_GGP
-    is_linux = $IS_LINUX
-}
-EOF
-if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
-cat >> out/${TARGET}/args.gn << EOF
-declare_args() {
-    cflags_cxx = [ "-stdlib=libc++" ]
-}
-EOF
-fi
-gn gen out/${TARGET}
+# TODO: -stdlib=libc++
+gn args out/${TARGET} --args="target_cpu=\"x64\" is_debug=$IS_DEBUG angle_enable_gl=false angle_enable_null=false angle_enable_vulkan=true is_ggp=$IS_GGP is_linux=$IS_LINUX
