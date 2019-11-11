394c10e73ec8f61fbeba56d59c61229738fa588c
Ethan Lee <flibitijibibo@gmail.com>
2019-11-11 14:32:15 -0500

    ANGLE already forces clang, just force the stdlib

--- confstrap.sh
+++ confstrap.sh
@@ -4,15 +4,11 @@ TARGET="${1}"
 IS_GGP="false"
 IS_LINUX="true"
 IS_DEBUG="false"
-OUR_CC="gcc"
-OUR_CXX="g++"
 
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
     IS_GGP="true"
     IS_LINUX="false"
-    OUR_CC="clang"
-    OUR_CXX="clang++ -stdlib=libc++"
 fi
 
 cd angle
@@ -23,7 +19,10 @@ is_debug = $IS_DEBUG
 angle_enable_gl = false
 angle_enable_null = false
 is_ggp = $IS_GGP
-cc = $OUR_CC
-cxx = $OUR_CXX
 EOF
+if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
+cat >> out/${TARGET}/args.gn << EOF
+cflags_cxx = "-stdlib=libc++"
+EOF
+fi
 gn gen out/${TARGET}
