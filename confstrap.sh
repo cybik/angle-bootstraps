790f0f794e2555e26b6549e86eda7afc65189a38
Renaud Lepage <cybik@users.noreply.github.com>
2019-11-11 19:37:27 -0800

    Reverting

--- confstrap.sh
+++ confstrap.sh
@@ -12,6 +12,19 @@ if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
 fi
 
 cd angle
+
+# Gen for the first time
 gn gen out/${TARGET}
-# TODO: -stdlib=libc++
-gn args out/${TARGET} --args="target_cpu=\"x64\" is_debug=$IS_DEBUG angle_enable_gl=false angle_enable_null=false angle_enable_vulkan=true is_ggp=$IS_GGP is_linux=$IS_LINUX"
+
+# Edit args, per https://gn.googlesource.com/gn/+/master/docs/reference.md#cmd_args
+cat >> out/${TARGET}/args.gn << EOF
+target_cpu = "x64"
+is_debug = $IS_DEBUG
+angle_enable_gl = false
+angle_enable_null = false
+is_ggp = $IS_GGP
+is_linux = $IS_LINUX
+EOF
+
+# Re-gen
+gn gen out/${TARGET}
