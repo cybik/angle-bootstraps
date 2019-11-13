f2a145655d1f1c3d28fb1db7253b7843fd93c430
Renaud Lepage <root@cybikbase.com>
2019-11-12 18:14:59 -0800

    types creation, also hey this is fixed now?

--- confstrap.sh
+++ confstrap.sh
@@ -5,13 +5,24 @@ IS_GGP="false"
 IS_LINUX="true"
 IS_DEBUG="false"
 
+cd angle
+
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
     IS_GGP="true"
     IS_LINUX="false"
+    # Config
+    mkdir -p include/ggp_c
+    cat > include/ggp_c/vulkan_types.h << EOF
+typedef uint64_t GgpFrameToken;
+typedef uint32_t GgpStreamDescriptor;
+static const uint32_t kGgpPrimaryStreamDescriptor = 1;
+EOF
+    echo "Created include/ggp_c/vulkan_types.h"
+else
+    [ -f "include/ggp_c/vulkan_types.h" ] && bash -c 'rm -f include/ggp_c/vulkan_types.h; echo "ggp_c vulkan_types deleted"'
 fi
 
-cd angle
 
 # Gen for the first time
 gn gen out/${TARGET}
