6f7470da7fba1498d21c9a5b40b4d35a50e26d26
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 11:03:09 -0500

    Commits += 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 export DEPOT_TOOLS=`pwd`/depot_tools
 export PATH=$DEPOT_TOOLS:$PATH
+
 cd angle
 scripts/bootstrap.py
 gclient sync
