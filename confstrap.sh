44a06d2a3cfea871bff95b430acf18da9777574f
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 22:54:52 -0500

    angle_use_x11 should disable itself now

--- confstrap.sh
+++ confstrap.sh
@@ -7,11 +7,7 @@ IS_DEBUG="false"
 
 # Either way it's Linux, but is this GGP or Desktop?
 IS_GGP="false"
-USE_X11="true"
-if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
-    IS_GGP="true"
-    USE_X11="false"
-fi
+[ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
 
 cd angle
-gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} angle_use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
+gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false angle_swiftshader=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP}
