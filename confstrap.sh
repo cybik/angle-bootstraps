fd093333bb512ea03ba3150e803949187019b9ee
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 20:31:55 -0500

    angle_use_x11

--- confstrap.sh
+++ confstrap.sh
@@ -13,4 +13,4 @@ if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
 fi
 
 cd angle
-gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
+gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} angle_use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
