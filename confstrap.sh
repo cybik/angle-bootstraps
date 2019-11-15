f08248a2e4ae36f5a06e44f431ca74e681deb6c1
Ethan Lee <flibitijibibo@gmail.com>
2019-11-15 12:32:52 -0500

    Pile of cleanup

--- confstrap.sh
+++ confstrap.sh
@@ -10,3 +10,11 @@ IS_GGP="false"
 
 cd angle
 gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false angle_swiftshader=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP}"
+
+# This header has to be in Vulkan-Headers!
+mkdir thirdparty/vulkan-headers/src/include/ggp_c
+cat > thirdparty/vulkan-headers/src/include/ggp_c/vulkan_types.h << EOF
+typedef uint64_t GgpFrameToken;
+typedef uint32_t GgpStreamDescriptor;
+static const uint32_t kGgpPrimaryStreamDescriptor = 1;
+EOF
