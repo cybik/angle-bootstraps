914c91a8424eebe2cd04901bb23245a8ff17b6fd
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 20:11:11 -0500

    Explicitly set use_x11

--- confstrap.sh
+++ confstrap.sh
@@ -6,7 +6,11 @@ IS_DEBUG="false"
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 
 IS_GGP="false"
-[ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
+USE_X11="true"
+if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
+    IS_GGP="true"
+    USE_X11="false"
+fi
 
 cd angle
-gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
+gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
