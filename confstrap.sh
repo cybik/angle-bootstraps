60739a4a3e0ff6a69b204158f80e61a394c47369
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 20:06:45 -0500

    Cleanup confstrap a bunch

--- confstrap.sh
+++ confstrap.sh
@@ -1,17 +1,12 @@
 #! /bin/bash
 
 TARGET="${1}"
-IS_GGP="false"
-IS_LINUX="true"
-IS_DEBUG="false"
-GN_TARGETOS="linux"
 
+IS_DEBUG="false"
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
-if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
-    IS_GGP="true"
-    IS_LINUX="false"
-    GN_TARGETOS="ggp"
-fi
+
+IS_GGP="false"
+[ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
 
 cd angle
-gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} is_desktop_linux=${IS_LINUX} target_os=\"${GN_TARGETOS}\" use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
+gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
