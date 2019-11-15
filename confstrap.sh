15188525d7e8119917a2ff04551ec533aa3807c6
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 23:48:00 -0500

    Revert "Commits += 1"
    This reverts commit dda5471cccfb83130117d5f172e1e2c7c9786fbd.

--- confstrap.sh
+++ confstrap.sh
@@ -5,6 +5,7 @@ TARGET="${1}"
 IS_DEBUG="false"
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 
+# Either way it's Linux, but is this GGP or Desktop?
 IS_GGP="false"
 [ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
 
