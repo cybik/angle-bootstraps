c02d26d9cd7f3689fce2e60366b24c8a73e3cdca
Ethan Lee <flibitijibibo@gmail.com>
2019-11-15 15:00:36 -0500

    Comment

--- confstrap.sh
+++ confstrap.sh
@@ -9,6 +9,7 @@ IS_DEBUG="false"
 IS_GGP="false"
 [ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
 
+# Generate project files
 cd angle
 gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false angle_swiftshader=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP}"
 
