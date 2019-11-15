b8a02f039e2be8691a35b161334468739592c7f8
Ethan Lee <flibitijibibo@gmail.com>
2019-11-14 20:31:55 -0500

    angle_use_x11

--- confstrap.sh
+++ confstrap.sh
@@ -5,6 +5,7 @@ TARGET="${1}"
 IS_DEBUG="false"
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 
+# Either way it's Linux, but is this GGP or Desktop?
 IS_GGP="false"
 USE_X11="true"
 if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
@@ -13,4 +14,4 @@ if [ ! -z "${2}" ] && [ "${2}" == "ggp" ]; then
 fi
 
 cd angle
-gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
+gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false is_ggp=${IS_GGP} angle_use_x11=${USE_X11} use_custom_libcxx=${IS_GGP} angle_swiftshader=false"
