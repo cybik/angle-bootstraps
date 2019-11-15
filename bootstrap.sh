3825a708c6bf9b12b761cffee1e0bc6af044885f
Ethan Lee <flibitijibibo@gmail.com>
2019-11-15 12:32:52 -0500

    Pile of cleanup and triggers, and a comment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Cloning
-git clone https://github.com/FNA-XNA/angle-ggp angle
+git clone https://github.com/ANGLE-Builds/angle-ggp angle
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 export DEPOT_TOOLS=`pwd`/depot_tools
--- confstrap.sh
+++ confstrap.sh
@@ -5,8 +5,18 @@ TARGET="${1}"
 IS_DEBUG="false"
 [ "${TARGET}" == "Debug" ] && IS_DEBUG="true"
 
+# Either way it's Linux, but is this GGP or Desktop?
 IS_GGP="false"
 [ ! -z "${2}" ] && [ "${2}" == "ggp" ] && IS_GGP="true"
 
+# Generate project files
 cd angle
 gn gen out/${TARGET} --args="target_cpu=\"x64\" is_debug=${IS_DEBUG} angle_enable_gl=false angle_enable_null=false angle_swiftshader=false is_ggp=${IS_GGP} use_custom_libcxx=${IS_GGP}"
+
+# This header has to be in Vulkan-Headers!
+mkdir third_party/vulkan-headers/src/include/ggp_c
+cat > third_party/vulkan-headers/src/include/ggp_c/vulkan_types.h << EOF
+typedef uint64_t GgpFrameToken;
+typedef uint32_t GgpStreamDescriptor;
+static const uint32_t kGgpPrimaryStreamDescriptor = 1;
+EOF
