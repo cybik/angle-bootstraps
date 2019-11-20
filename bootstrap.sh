7935b3496a78bbb432ea8fc5d2618707d1e5b0ea
Ethan Lee <flibitijibibo@gmail.com>
2019-11-19 19:53:55 -0500

    Move vulkan_types.h to bootstrip, fix git apply directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,10 @@
 git clone https://chromium.googlesource.com/angle/angle
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
+export BUILDROOT=`pwd`
+
 # Add depot_tools to PATH, for gclient/gn and friends
-export DEPOT_TOOLS=`pwd`/depot_tools
-export PATH=$DEPOT_TOOLS:$PATH
+export PATH=$BUILDROOT/depot_tools:$PATH
 
 # Download everything else
 cd angle
@@ -15,6 +16,14 @@ gclient sync
 sudo ./build/install-build-deps.sh
 
 # Apply custom patches for GGP public builds
-git apply ../patches/0001-HACK-Force-__ggp__-define.patch
-git --git-dir=third_party/vulkan-loader/src/.git apply ../patches/0001-HACK-Stub-GGP-functions.patch
-git --git-dir=third_party/vulkan-headers/src/.git apply ../patches/0001-GN-Make-PLATFORM_XCB-optional-based-on-a-user-define.patch
+git apply $BUILDROOT/patches/0001-HACK-Force-__ggp__-define.patch
+git -C third_party/vulkan-loader/src apply $BUILDROOT/patches/0001-HACK-Stub-GGP-functions.patch
+git -C third_party/vulkan-headers/src apply $BUILDROOT/patches/0001-GN-Make-PLATFORM_XCB-optional-based-on-a-user-define.patch
+
+# This header has to be in Vulkan-Headers!
+mkdir third_party/vulkan-headers/src/include/ggp_c
+cat > third_party/vulkan-headers/src/include/ggp_c/vulkan_types.h << EOF
+typedef uint64_t GgpFrameToken;
+typedef uint32_t GgpStreamDescriptor;
+static const uint32_t kGgpPrimaryStreamDescriptor = 1;
+EOF
