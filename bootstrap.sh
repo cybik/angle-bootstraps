b2aa91b727b1709a6bcf7589f053a223c5dc35c1
Ethan Lee <flibitijibibo@gmail.com>
2019-11-19 19:13:50 -0500

    Go back to mainline repo, with custom patches for GGP hacks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
 # Cloning
-git clone https://github.com/ANGLE-Builds/angle-ggp angle
+git clone https://chromium.googlesource.com/angle/angle
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
+# Add depot_tools to PATH, for gclient/gn and friends
 export DEPOT_TOOLS=`pwd`/depot_tools
 export PATH=$DEPOT_TOOLS:$PATH
+
+# Download everything else
 cd angle
 scripts/bootstrap.py
 gclient sync
 sudo ./build/install-build-deps.sh
+
+# Apply custom patches for GGP public builds
+git am ../patches/0001-HACK-Force-__ggp__-define.patch
+git --git-dir=third_party/vulkan-loader/src/.git am ../patches/0001-HACK-Stub-GGP-functions.patch
+git --git-dir=third_party/vulkan-headers/src/.git am ../patches/0001-GN-Make-PLATFORM_XCB-optional-based-on-a-user-define.patch
