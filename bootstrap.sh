e15d693d5fb06b5070e572e2ba29fef876dfa6b0
Ethan Lee <flibitijibibo@gmail.com>
2019-11-20 01:14:50 -0500

    Revert "Add -v to git apply"
    This reverts commit b4e142bede2390309c7f960f4f72fc19d20b24d0.

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,9 +16,9 @@ gclient sync
 sudo ./build/install-build-deps.sh
 
 # Apply custom patches for GGP public builds
-git apply -v $BUILDROOT/patches/0001-HACK-Force-__ggp__-define.patch
-git -C third_party/vulkan-loader/src apply -v $BUILDROOT/patches/0001-HACK-Stub-GGP-functions.patch
-git -C third_party/vulkan-headers/src apply -v $BUILDROOT/patches/0001-GN-Make-PLATFORM_XCB-optional-based-on-a-user-define.patch
+git apply $BUILDROOT/patches/0001-HACK-Force-__ggp__-define.patch
+git -C third_party/vulkan-loader/src apply $BUILDROOT/patches/0001-HACK-Stub-GGP-functions.patch
+git -C third_party/vulkan-headers/src apply $BUILDROOT/patches/0001-GN-Make-PLATFORM_XCB-optional-based-on-a-user-define.patch
 
 # This header has to be in Vulkan-Headers!
 mkdir third_party/vulkan-headers/src/include/ggp_c
