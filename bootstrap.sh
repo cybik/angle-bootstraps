fe937cb433e27c18a48f6b67a6c143f4465f59e3
Ethan Lee <flibitijibibo@gmail.com>
2019-12-31 01:39:48 -0500

    Testing the idea of skipping vulkan-loader entirely

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ sudo ./build/install-build-deps.sh
 # Apply custom patches for GGP public builds
 git apply $BUILDROOT/patches/0001-HACK-Force-__ggp__-define.patch
 git apply $BUILDROOT/patches/XXXX-Fix-GGP-build.patch
-git -C third_party/vulkan-loader/src apply $BUILDROOT/patches/0001-HACK-Stub-GGP-functions.patch
+git apply $BUILDROOT/patches/XXXX-Ignore-vulkan-loader-dep-for-GGP.patch
 
 # This header has to be in Vulkan-Headers!
 mkdir third_party/vulkan-headers/src/include/ggp_c
